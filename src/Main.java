5674ec92c830ffa7e7369a1fecaee4aa8b60c279
marc2 <marc2@uni-bremen.de>
2018-12-18 21:45:34 +0100

    Erstmal ein Adressbuch zum Äufwärmen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.awt.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+    {
+        System.out.println(">>> Willkommen im Adressbuch!");
     }
+
 }
