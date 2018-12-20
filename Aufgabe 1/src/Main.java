fff8eafcac03060cd230837141c39493ae0d3909
marc2 <marc2@uni-bremen.de>
2018-12-20 15:16:04 +0100

    Aufgabe 1.e fertig

--- Aufgabe 1/src/Main.java
+++ Aufgabe 1/src/Main.java
@@ -1,24 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main
 {
 
-    //static private ArrayList<Integer> list = new ArrayList<Integer>();
-
-    static private int[] list = new int[] {5, 3, 101, 100};
+    static private ArrayList<Integer> list = new ArrayList<Integer>();
 
     public static void main(String[] args)
     {
         System.out.println(maximum(list));
     }
 
-    static int maximum(final int[] list)
+    static int maximum(final ArrayList<Integer> list)
     {
-        int max = list[0];
-        for(int i : list)
+        list.add(3);
+        list.add(101);
+        list.add(5);
+        list.add(100);
+        list.add(99);
+
+        Iterator<Integer> it = list.iterator();
+        int max = it.next();
+        while(it.hasNext())
         {
-            if(i  > max)
+            int i = it.next();
+            if(i > max)
             {
                 max = i;
             }
