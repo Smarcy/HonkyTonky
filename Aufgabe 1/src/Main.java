a9caaa608befdab7df7ccad6fe71a67b2f4b1acd
marc2 <marc2@uni-bremen.de>
2018-12-20 14:45:40 +0100

    Aufgabe 1.a fertig

--- Aufgabe 1/src/Main.java
+++ Aufgabe 1/src/Main.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main
 {
+
+    static private ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public static void main(String[] args)
+    {
+        list.add(5);
+        list.add(3);
+        list.add(101);
+        list.add(100);
+
+        System.out.println(maximum(list));
+    }
+
+    static int maximum(final List<Integer> list)
+    {
+        int max = list.get(0);
+        for(int i = list.size()-1; i >= 0; i--)
+        {
+            if(list.get(i)  > max)
+            {
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
 }
