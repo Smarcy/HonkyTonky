25d9e59202fa3fb24021c40f31bd41aedda0b1a8
Marc Asendorf <marc2@uni-bremen.de>
2018-12-20 12:04:37 +0100

    Aufgabe 1 angefangen

---
+++ PI1 - Arbeitsblatt 2/Aufgabe 1/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+    {
+
+    static private ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public static void main(String[] args)
+    {
+        list.add(5);
+        list.add(3);
+        list.add(101);
+        list.add(100);
+
+       System.out.println(maximum(list));
+    }
+
+static int maximum(final List<Integer> list)
+    {
+    int max = list.get(0);
+    for(int i = 1; i < list.size(); ++i)
+        {
+        if(list.get(i)  > max)
+            {
+            max = list.get(i);
+            }
+        }
+    return max;
+    }
+}
