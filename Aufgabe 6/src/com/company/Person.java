f44f1ac1c4f04b8708e98011c572ff864319fc95
marc2 <marc2@uni-bremen.de>
2018-12-21 14:44:51 +0100

    Aufgabe 6 fertig

---
+++ Aufgabe 6/src/com/company/Person.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.*;
+
+public class Person
+{
+    String name;
+
+    Person(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+
+
+}
