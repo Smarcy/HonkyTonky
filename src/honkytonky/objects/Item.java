09a11c76ae8448ecfc3ed56e80106530e2d6ae49
marc2 <marc2@uni-bremen.de>
2019-01-10 16:54:11 +0100

    Item & Weapon structures

--- src/honkytonky/objects/Item.java
+++ src/honkytonky/objects/Item.java
@@ -4,5 +4,12 @@ package honkytonky.objects;
 
 public class Item
 {
-    
+    private int id;
+    private String name;
+
+    Item(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/honkytonky/objects/Weapon.java
+++ src/honkytonky/objects/Weapon.java
@@ -2,5 +2,26 @@ package honkytonky.objects;
 
 public class Weapon extends Item
 {
+    enum WeaponType
+    {
+        SWORD,
+        AXE,
+        HAMMER
+    }
 
+    private int damage;
+    private int durability;
+    private boolean twohanded; // false = onehanded, true = twohanded
+    private WeaponType weaponType;
+
+    public Weapon(int id, String name, WeaponType weaponType, int damage, int durability, boolean twohanded)
+    {
+        super(id, name);
+
+        this.weaponType = weaponType;
+        this.damage = damage;
+        this.durability = durability;
+        this.twohanded = twohanded;
+
+    }
 }
