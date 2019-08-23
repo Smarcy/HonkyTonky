8d6acab8d2cda6e644178b99879010a605a7fbf9
Marc A <marc2@uni-bremen.de>
2019-08-24 00:42:05 +0200

    simplified CharacterInfoPattern

--- src/main/java/honkytonky/misc/CharacterInfoPattern.java
+++ src/main/java/honkytonky/misc/CharacterInfoPattern.java
@@ -7,24 +7,25 @@ import honkytonky.objects.Player;
 
 public class CharacterInfoPattern {
 
+    Player player;
+
     public void printCharacterInfo(Player player) {
-        printNameInfo(player);
-        printHealthInfo(player);
-        printLevelInfo(player);
-        printExperienceInfo(player);
-        printGoldInfo(player);
-        printWeaponInfo(player);
-        printArmorInfo(player);
-        printLocationInfo(player);
+        this.player = player;
+        printNameInfo();
+        printHealthInfo();
+        printLevelInfo();
+        printExperienceInfo();
+        printGoldInfo();
+        printWeaponInfo();
+        printArmorInfo();
+        printLocationInfo();
     }
 
     /**
      * prints info for the type and strength (armorpoints) of the player's current armor Output
      * pattern: "Your Current Armor Is: ArmorType (x Armor Points)"
-     *
-     * @param player the playerobject that is wearing the armor
      */
-    private void printArmorInfo(Player player) {
+    private void printArmorInfo() {
         if (player.getArmor().getArmorPoints() <= 1) {
             System.out.println("Armor: " + ANSI_YELLOW + String
               .format("%c[%d;%df", 0x1B, 7, 15) + player.getArmor()
@@ -41,10 +42,8 @@ public class CharacterInfoPattern {
     /**
      * prints info for the type and strength (damage) of the player's current weapon Output pattern:
      * "Your Current Weapon Is: WeaponType (x Damage)"
-     *
-     * @param player the playerobject that is carrying the weapon
      */
-    private void printWeaponInfo(Player player) {
+    private void printWeaponInfo() {
         System.out.println(
           "Weapon: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 6, 15)
             + player.getWeapon() + " (" + player.getWeapon().getDamage() + " Damage)" + ANSI_RESET);
@@ -52,10 +51,8 @@ public class CharacterInfoPattern {
 
     /**
      * print info for the current Location (read: room) of the player
-     *
-     * @param player the playerobject thats location is printed
      */
-    private void printLocationInfo(Player player) {
+    private void printLocationInfo() {
         System.out.println(
           "Location: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 8, 15)
             + player.getCurrentRoom() + ANSI_RESET);
@@ -64,10 +61,8 @@ public class CharacterInfoPattern {
     /**
      * prints info for the current and maximal health of the player Output pattern: Your Current HP:
      * (currentHP) / (maximalHP)
-     *
-     * @param player the playerobject thats health is printed
      */
-    private void printHealthInfo(Player player) {
+    private void printHealthInfo() {
         System.out
           .println("Health: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 2, 15)
                      + player.getHp() + " / " + player.getMaxHP() + ANSI_RESET);
@@ -75,28 +70,26 @@ public class CharacterInfoPattern {
 
     /**
      * prints info for the name of the player
-     *
-     * @param player the playerobject thats name is printed
      */
-    private void printNameInfo(Player player) {
+    private void printNameInfo() {
         System.out.println(
           "Name: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 1, 15) + player
             + ANSI_RESET);
     }
 
-    private void printLevelInfo(Player player) {
+    private void printLevelInfo() {
         System.out.println(
           "Level: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 3, 15) + player
             .getLevel() + ANSI_RESET);
     }
 
-    private void printExperienceInfo(Player player) {
+    private void printExperienceInfo() {
         System.out.println(
           "Experience: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 4, 15) + player
             .getExperience() + " (" + player.getPercentalExperience() + "%)" + ANSI_RESET);
     }
 
-    private void printGoldInfo(Player player) {
+    private void printGoldInfo() {
         System.out.println(
           "Gold: " + ANSI_YELLOW + String.format("%c[%d;%df", 0x1B, 5, 15) + player
             .getGold() + ANSI_RESET);
