cb5b481b964d06750bf1d03bd2cc3d6545ef2b9b
Marc A <marc2@uni-bremen.de>
2019-08-24 12:57:39 +0200

    add Trade with Merchants #changelog

--- src/main/java/honkytonky/controller/MerchantDialogController.java
+++ src/main/java/honkytonky/controller/MerchantDialogController.java
@@ -4,6 +4,7 @@ import static honkytonky.misc.ANSI_Color_Codes.ANSI_RESET;
 import static honkytonky.misc.ANSI_Color_Codes.ANSI_YELLOW;
 import static honkytonky.misc.ClearScreen.clearScreen;
 
+import honkytonky.objects.Item;
 import honkytonky.objects.Merchant;
 import honkytonky.objects.Player;
 import java.util.Scanner;
@@ -11,13 +12,16 @@ import java.util.Scanner;
 public class MerchantDialogController {
 
     private final Scanner scanner = new Scanner(System.in);
+    Player player;
+    Merchant merchant;
 
     void printMerchantDialog(Player player) {
+        this.player = player;
         Boolean run = true;
 
         while(run) {
             clearScreen();
-            Merchant merchant = player.getCurrentRoom().getPresentMerchant();
+            this.merchant = player.getCurrentRoom().getPresentMerchant();
 
             System.out.println("Hello, my name is " + ANSI_YELLOW + merchant + ANSI_RESET + "\n");
             System.out.println("1) Talk to " + merchant);
@@ -32,9 +36,10 @@ public class MerchantDialogController {
                     scanner.nextLine();
                     break;
                 case 2:
-                    merchant.printItemsForSell();
-                    System.out.print("\n\nContinue..");
-                    scanner.nextLine();
+                    int choice = merchant.printItemsForSell();
+                    if(choice != -1) {
+                        tradeWithMerchant(choice);
+                    }
                     break;
                 case 3:
                     break;
@@ -43,5 +48,11 @@ public class MerchantDialogController {
                     break;
             }
         }
+    }
+
+    private void tradeWithMerchant(int choice) {
+        Item itemToBuy = merchant.getItemsForSell().get(choice - 1);
+        player.getInventory().add(itemToBuy);
+        merchant.removeItemFromShop(itemToBuy);
     }
 }
--- src/main/java/honkytonky/objects/Merchant.java
+++ src/main/java/honkytonky/objects/Merchant.java
@@ -7,6 +7,7 @@ import static honkytonky.misc.ANSI_Color_Codes.ANSI_RESET;
 import honkytonky.misc.ClearScreen;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -16,6 +17,7 @@ public class Merchant extends Actor {
 
     private final List<Item> itemsForSell = new ArrayList<>();
     private final String smalltalk;
+    Scanner scanner = new Scanner(System.in);
 
     public Merchant(String name, int maxHP, int level, String smalltalk) {
         super(name, maxHP, level);
@@ -35,11 +37,33 @@ public class Merchant extends Actor {
         System.out.println(ANSI_CYAN + this.getName() + ": " + ANSI_BLUE + smalltalk + ANSI_RESET);
     }
 
-    public void printItemsForSell() {
+    /**
+     *
+     * @return ID of item player wants to buy
+     */
+    public int printItemsForSell() {
+        ClearScreen.clearScreen();
+
+        System.out.println(ANSI_CYAN + this.getName() + ANSI_RESET + " is selling the following items:\n");
+
         int i = 1;  //start counting at 1 for design purposes
         for(Item item : itemsForSell) {
-            System.out.println(i + ") " + item + "\n");
+            System.out.println(i + ") " + item);
+            i++;
         }
+
+        System.out.println(i + ") Don't trade \n");
+        final int choice = Integer.parseInt(scanner.nextLine());
+
+        // Can't use variable in switch-Statement, therefore a specialcase for choosing item
+        if(choice == i) {   // choose "Dont trade"
+            return -1;
+        } else if(choice < i && choice > 0) {     // choice does match an item
+            return choice;
+        } else {    // choice does not match with options -> print Dialog again
+            this.printItemsForSell();
+        }
+        return -1;
     }
 
     @Override
