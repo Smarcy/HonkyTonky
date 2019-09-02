40e17fa7ddc34e8152a77190b35a9076745be4e1
Marc <marc2@uni-bremen.de>
2019-09-02 19:50:48 +0200

    further Singleton finetuning

--- src/main/java/honkytonky/factories/CreateWorld.java
+++ src/main/java/honkytonky/factories/CreateWorld.java
@@ -6,20 +6,6 @@ public class CreateWorld {
      * reads all needed CSV files to populate the World with Monsters, Doors, Merchants
      */
     public static void populateWorld() {
-        //@formatter:off
-       // roomFactory            = new RoomFactory();
-        //doorFactory            = new DoorFactory();
-        //monsterFactory         = new MonsterFactory();
-        //merchantFactory        = new MerchantFactory();
-        //weaponFactory          = new WeaponFactory();
-        //armorFactory           = new ArmorFactory();
-        //potionFactory          = new PotionFactory();
-        //playerDialogController = new PlayerDialogController();
-        //merchantController     = new MerchantController();
-       // battleController       = new BattleController();
-       // playerController       = new PlayerController();
-        //@formatter:on
-
         RoomFactory.getInstance().createRooms();
         DoorFactory.getInstance().createDoors();
         MonsterFactory.getInstance().createMonsters();
@@ -27,16 +13,15 @@ public class CreateWorld {
         ArmorFactory.getInstance().createArmors();
         PotionFactory.getInstance().createPotions();
         MerchantFactory.getInstance().createMerchants();
-
     }
 
     /**
      * delete everything in the world - only useable if the very next methodcall is 'populateWorld()' !!!
      */
     public static void wipeWorld() {
-//        doorFactory.wipeDoors();
-//        monsterFactory.wipeMonsters();
-//        merchantFactory.wipeMerchants();
-//        roomFactory.wipeRooms();
+        DoorFactory.getInstance().wipeDoors();
+        MonsterFactory.getInstance().wipeMonsters();
+        MerchantFactory.getInstance().wipeMerchants();
+        RoomFactory.getInstance().wipeRooms();
     }
 }
